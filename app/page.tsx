e1d476a51c50ddf8d04ebf5e29a1f0c6d601c70a
kubawaso <159401490+kubawaso@users.noreply.github.com>
2025-01-10 14:26:32 -0500

    Update page title

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-        <h1 className="text-4xl font-bold mb-8">YouTube Title Bandit</h1>
+        <h1 className="text-4xl font-bold mb-8">YouTube Title Optimizer - Multi-Armed Bandit</h1>
 
         <div className="mb-8">
           <button 
